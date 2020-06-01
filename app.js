b4a768fea80195ab8bedf3705ddcb0342e130cce
Taranti <38500851+Taranti@users.noreply.github.com>
2020-06-01 12:15:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use(cors())
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Lottery!')
 })
 
 app.listen(3000, function () {
